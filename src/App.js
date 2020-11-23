12a36d81e0d6fcd7ffa6dc8d4c5fa8bbd656debf
lukabelani <luka.belani@gmail.com>
2020-11-23 13:04:08 +0100

    refix typo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const SpeechRecognition =
-  window.speechRecognition || window.webkitSpeechRecognition;
+  window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const mic = new SpeechRecognition();
 
